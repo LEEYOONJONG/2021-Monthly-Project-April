e20e96f54d8051d47250473d9c4148052acf7a33
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-04-07 15:30:11 +0900

    HeaderView, CollectionView 추가

--- 2021_April/ViewController.swift
+++ 2021_April/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ScoreCell", for: indexPath) as? ScoreCell else { return UICollectionViewCell() }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "ScoreCellHeaderView", for: indexPath)
+            return headerView
+        default:
+            assert(false, "응 아니야") }
+
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+class ScoreCell: UICollectionViewCell{
+    @IBOutlet weak var semesterLabel:UILabel!
+    @IBOutlet weak var scoreLabel:UILabel!
+    
+}
+
+class ScoreCellHeaderView: UICollectionReusableView{
+    
+    @IBOutlet weak var headerLabel: UILabel!
+}
