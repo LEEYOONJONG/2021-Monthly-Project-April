bf44df10664258cf638f26b7c137a55475bc3238
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-04-08 22:37:04 +0900

    textField의 text를 DetailViewController의 배열에 입력하는 것까지 완료. 데이터 저장 필요

--- 2021_April/DetailViewController.swift
+++ 2021_April/DetailViewController.swift
@@ -7,19 +7,29 @@
 
 import UIKit
 
+
+
 class DetailViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var DetailCollectionView: UICollectionView!
     
     var semester:String?
     
+    var subjects:[String]=["","","","","","","",""]
+    var points:[Double]=[0,0,0,0,0,0,0,0]
+    var grades:[Double]=[0,0,0,0,0,0,0,0]
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 8
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DetailCell", for: indexPath) as? DetailCell else { return UICollectionViewCell() }
+        
+        
         return cell
     }
+    
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
         case UICollectionView.elementKindSectionHeader:
@@ -31,17 +41,31 @@ class DetailViewController: UIViewController, UICollectionViewDelegate, UICollec
 
         
     }
-
+    func printall(){
+        print(semester)
+        for cell in DetailCollectionView.visibleCells as! [DetailCell]{
+            let indexPath = DetailCollectionView.indexPath(for: cell)
+//            print(indexPath, cell.gradeInput.text ?? "", cell.pointInput.text ?? "", cell.subjectInput.text ?? "")
+            subjects[indexPath!.item] = cell.subjectInput.text ?? ""
+            points[indexPath!.item] = Double(cell.pointInput.text!) ?? 0
+            grades[indexPath!.item] = Double(cell.gradeInput.text!) ?? 0
+        }
+    }
     
     
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        printall()
+        print(subjects)
+        print(points)
+        print(grades)
     }
 }
 
